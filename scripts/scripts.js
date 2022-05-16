088103b1bc67de1870ab92d5afb8f041ef79ec14
seeyd7 <szymon8866@gmail.com>
2022-05-16 22:40:54 +0200

    Opinie koniec chyba

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+const go2Top = document.querySelector("#go2Top");
 const menu = document.querySelector("#menu");
 
 const navMenu = document.querySelector(".navbar-menu");
@@ -6,15 +7,27 @@ const menuToggler = document.querySelector(".menu-toggler");
 const navbar = document.querySelector("nav");
 
 
-menuToggler.addEventListener("click", () => {
-	if(menuToggler.classList.contains("active")) {
-		menuToggler.classList.remove("active");
-		navMenu.classList.remove("active");
+addEventListener("scroll", () => {
+	let scroll = this.scrollY;
+	if(scroll > menu.clientHeight) {
+    go2Top.style.opacity = 1;
+    go2Top.style.lineHeight = "40px";
+		go2Top.style.cursor = "pointer";
+		console.log(go2Top.style.cursor);
+	navbar.style.height = "8vh";
+	navbar.style.backgroundColor = "rgba(0, 0, 0, 0.75)"
 	} else {
-		menuToggler.classList.add("active");
-		navMenu.classList.add("active");
+		go2Top.style.opacity = 0;
+		go2Top.style.cursor = "default";
+		navbar.style.height = "10vh";
+		navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+		console.log(go2Top.style.cursor);
 	}
-});
+})
+
+go2Top.addEventListener("click", () => {
+	if(go2Top.style.opacity != 0) window.scrollTo(0, 0);
+})
 
 function showSlides(n) {
 	var i;
@@ -43,21 +56,14 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-addEventListener("scroll", () => {
-	let scroll = this.scrollY;
-	if(scroll > menu.clientHeight) {
-    go2Top.style.opacity = 1;
-    go2Top.style.lineHeight = "40px";
-		go2Top.style.cursor = "pointer";
-		console.log(go2Top.style.cursor);
+menuToggler.addEventListener("click", () => {
+	if(menuToggler.classList.contains("active")) {
+		menuToggler.classList.remove("active");
+		navMenu.classList.remove("active");
 	} else {
-		go2Top.style.opacity = 0;
-		go2Top.style.cursor = "default";
-		console.log(go2Top.style.cursor);
+		menuToggler.classList.add("active");
+		navMenu.classList.add("active");
 	}
-})
+});
 
-go2Top.addEventListener("click", () => {
-	if(go2Top.style.opacity != 0) window.scrollTo(0, 0);
-})
 
